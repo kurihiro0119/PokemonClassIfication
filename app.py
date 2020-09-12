06f3fb25d9aadb2b02ce31f0cad193a4c181317b
kurihiro0119 <54324711+kurihiro0119@users.noreply.github.com>
2020-09-12 14:35:40 +0900

    add

--- app.py
+++ app.py
@@ -60,4 +60,5 @@ def classify_img():
 
 # メインで実行される関数
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5009, debug=True)
+    # app.run(host='127.0.0.1', port=5010, debug=True)
+    app.run(debug=True)
